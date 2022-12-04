f23827eda8a9dc18d1a2ffa903c65a724b908ac3
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-12-05 01:12:33 +0900

    로그인 + 프로필사진 구현

--- backend/api/schemas/post.py
+++ backend/api/schemas/post.py
@@ -1,8 +1,8 @@
-import imp
 from api.ma import ma, MethodField
 from api.models.post import PostModel
 from api.models.user import UserModel
 from marshmallow import fields
+from api.schemas.user import AuthorSchema
 
 
 class PostSchema(ma.SQLAlchemyAutoSchema):
@@ -10,23 +10,12 @@ class PostSchema(ma.SQLAlchemyAutoSchema):
     게시물 모델에 관한 직렬화 규칙 정의
     """
     image = fields.String(required=True)
-
     created_at = fields.DateTime(format="%Y-%m-%d,%H:%M:%S")
     updated_at = fields.DateTime(format="%Y-%m-%d,%H:%M:%S")
-
-    author_name = MethodField("get_author_name")
-
-    def get_author_name(self, obj):
-        return obj.author.username
+    author = fields.Nested(AuthorSchema)
 
     class Meta:
         model = PostModel
-        dump_only = [
-            "author_name",
-        ]
-        # load_only = [
-        #     "author_id",
-        # ]
         exclude = ("author_id",)
         include_fk = True
         load_instance = True
